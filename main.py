e96d886c23dbc6d81f5a5219bbc93f7cdbe29608
Om Prabhu <omprabhu98@gmail.com>
2020-03-18 18:07:55 -0400

    Fixed bugs from previous commit.

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ def mappingf(string1,string2):
     if len(string1) != len(string2):
         return False
 
-    map = [[0] * 256] * 2
+    map = [([0] * 2) for row in range(256)]
+    for i in range(0,len(string2)):
+        if map[ord(string1[i])][0] == 0:
+            map[ord(string2[i])][1] = 1
+            map[ord(string1[i])][0] = string2[i]
 
-    for i in range(0,len(string2)-1):
-        if map[0][ord(string1[i])] == 0:
-            if map[1][ord(string2[i])] == 1:
-                print(ord(string2[i]))
-                return False
-            map[1][ord(string2[i])] = 1
-            map[0][ord(string1[i])] = string2[i]
-
-        elif map[0][ord(string1[i])] != string2[i]:
+        elif map[ord(string1[i])][0] != string2[i]:
             return False
 
     return True
